MotorolaMobilityLLC/kernel-msm

drivers/base: cpu: Add node for core control isolation
This cpu node can be used to check the isolated cores
which are made unavailable for scheduler and exempted
from waking up on interrupts or from running timers at
the given time based on the load. All the runnable tasks,
timers and hrtimers should have migrated to other online
running cores.

Change-Id: I32fed6cc131288b9c318f0dea65e2e59396225ca
Signed-off-by: Sabyasachi Singh <sssingh@codeaurora.org>

drivers/base/cpu.c
@@ -393,6 +393,7 @@ static struct cpu_attr cpu_attrs[] = {
 	_CPU_ATTR(online, &cpu_online_mask),
 	_CPU_ATTR(possible, &cpu_possible_mask),
 	_CPU_ATTR(present, &cpu_present_mask),
+	_CPU_ATTR(core_ctl_isolated, &cpu_isolated_mask),
 };
 
 /*
@@ -627,6 +628,7 @@ static struct attribute *cpu_root_attrs[] = {
 	&cpu_attrs[0].attr.attr,
 	&cpu_attrs[1].attr.attr,
 	&cpu_attrs[2].attr.attr,
+	&cpu_attrs[3].attr.attr,
 	&dev_attr_kernel_max.attr,
 	&dev_attr_offline.attr,
 	&dev_attr_isolated.attr,

tonzy-su/android_kernel_oppo_sm8150

Change-Id: I6771f53e59ca37b2cc3b17309cc8f4d75462cdfa
Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

drivers/base/cpu.c
@@ -255,6 +255,7 @@ static struct cpu_attr cpu_attrs[] = {
 	_CPU_ATTR(online, &__cpu_online_mask),
 	_CPU_ATTR(possible, &__cpu_possible_mask),
 	_CPU_ATTR(present, &__cpu_present_mask),
+	_CPU_ATTR(core_ctl_isolated, &__cpu_isolated_mask),
 };
 
 /*
@@ -491,6 +492,7 @@ static struct attribute *cpu_root_attrs[] = {
 	&cpu_attrs[0].attr.attr,
 	&cpu_attrs[1].attr.attr,
 	&cpu_attrs[2].attr.attr,
+	&cpu_attrs[3].attr.attr,
 	&dev_attr_kernel_max.attr,
 	&dev_attr_offline.attr,
 	&dev_attr_isolated.attr,