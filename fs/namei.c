235089eef17e3cd6b237454e3218e4c2992dddcf
tonzy-su <3521425739@qq.com>
2026-08-07 10:20:59 +0800

    fs: add vfs_mkobj for bpffs object pinning
    bpf_obj_do_pin() backport (0112: switch to vfs_mkobj) calls vfs_mkobj()
    which is missing in 4.14. Port vfs_mkobj() from upstream 4.19 stable:
    fs/namei.c implementation (may_create(NULL, dir, dentry) for the
    Qualcomm may_create(mnt, dir, dentry) variant + security_inode_create +
    callback + fsnotify_create) and include/linux/fs.h declaration.

--- fs/namei.c
+++ fs/namei.c
@@ -3885,6 +3885,28 @@ int vfs_mknod(struct inode *dir, struct dentry *dentry, umode_t mode, dev_t dev)
 }
 EXPORT_SYMBOL(vfs_mknod);
 
+int vfs_mkobj(struct dentry *dentry, umode_t mode,
+	      int (*f)(struct dentry *, umode_t, void *),
+	      void *arg)
+{
+	struct inode *dir = dentry->d_parent->d_inode;
+	int error = may_create(NULL, dir, dentry);
+
+	if (error)
+		return error;
+
+	mode &= S_IALLUGO;
+	mode |= S_IFREG;
+	error = security_inode_create(dir, dentry, mode);
+	if (error)
+		return error;
+	error = f(dentry, mode, arg);
+	if (!error)
+		fsnotify_create(dir, dentry);
+	return error;
+}
+EXPORT_SYMBOL(vfs_mkobj);
+
 static int may_mknod(umode_t mode)
 {
 	switch (mode & S_IFMT) {
